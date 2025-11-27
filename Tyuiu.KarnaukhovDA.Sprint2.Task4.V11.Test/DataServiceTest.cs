e1412a005c75eb496f0e728970e5d1fcf6408311
Даниил Карнаухов <Tyuiu.KarnaukhovDA@outlook.com>
2025-11-27 20:26:52 +0500

    Task4

---
+++ Tyuiu.KarnaukhovDA.Sprint2.Task4.V11.Test/DataServiceTest.cs
@@ -0,0 +1,28 @@
+﻿using Tyuiu.KarnaukhovDA.Sprint2.Task4.V11.Lib;
+namespace Tyuiu.KarnaukhovDA.Sprint2.Task4.V11.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 10;
+            double y = 2;
+            double res = ds.Calculate(x, y);
+            double wait = 9.486;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod] 
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 10;
+            double y = -121;
+            double res = ds.Calculate(x, y);
+            double wait = -121;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.KarnaukhovDA.Sprint2.Task4.V11/Program.cs
@@ -0,0 +1,40 @@
+﻿using Tyuiu.KarnaukhovDA.Sprint2.Task4.V11.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #2 | Выполнил: Карнаухов Д. А. | СМАРТб-25-1";
+        Console.WriteLine("*********************************************************************************************************");
+        Console.WriteLine("* Спринт #2                                                                                             *");
+        Console.WriteLine("* Тема: Тернарный оператор                                                                              *");
+        Console.WriteLine("* Задание #4                                                                                            *");
+        Console.WriteLine("* Вариант #11                                                                                           *");
+        Console.WriteLine("* Выполнил: Карнаухов Даниил Александрович | СМАРТб-25-1                                                *");
+        Console.WriteLine("*********************************************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                                              *");
+        Console.WriteLine("* Написать программу которая вычисляет требуемое значение с использованием тернарного оператора         *");
+        Console.WriteLine("* где пользователь вводит значение переменных x,y с клавиатуры                                          *");
+        Console.WriteLine("*                                                                                                       *");
+        Console.WriteLine("*********************************************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                                      *");
+        Console.WriteLine("*********************************************************************************************************");
+
+        Console.WriteLine("Введите значение X :");
+        double x = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите значение Y :");
+        double y = Convert.ToDouble(Console.ReadLine());
+
+        double res = ds.Calculate(x, y);
+
+        Console.WriteLine("*********************************************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                                                            *");
+        Console.WriteLine("*********************************************************************************************************");
+
+        Console.WriteLine("Значение функции =" + res);
+
+        Console.ReadKey();
+    }
+}
