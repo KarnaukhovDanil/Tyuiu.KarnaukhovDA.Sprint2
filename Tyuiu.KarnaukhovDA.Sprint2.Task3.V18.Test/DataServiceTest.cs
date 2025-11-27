ec76b36b35387515638374ca2f724d124822a025
Даниил Карнаухов <Tyuiu.KarnaukhovDA@outlook.com>
2025-11-27 17:04:33 +0500

    Task3

---
+++ Tyuiu.KarnaukhovDA.Sprint2.Task3.V18.Test/DataServiceTest.cs
@@ -0,0 +1,49 @@
+﻿using Tyuiu.KarnaukhovDA.Sprint2.Task3.V18.Lib;
+namespace Tyuiu.KarnaukhovDA.Sprint2.Task3.V18.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double res = ds.Calculate(x);
+            double wait = 36;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 0;
+            double res = ds.Calculate(x);
+            double wait = 0;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCondition3()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double res = ds.Calculate(x);
+            double wait = 2;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCondition4()
+        {
+            DataService ds = new DataService();
+            double x = - 100;
+            double res = ds.Calculate(x);
+            double wait = - 1099.99;
+            Assert.AreEqual(wait, res);
+        }
+        
+        
+    }
+}
