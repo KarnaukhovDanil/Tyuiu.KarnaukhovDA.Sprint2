2375b00178e3d3340ffb7a4ecea0accd52a317dc
Даниил Карнаухов <Tyuiu.KarnaukhovDA@outlook.com>
2026-01-19 20:43:48 +0500

    Task7

---
+++ Tyuiu.KarnaukhovDA.Sprint2.Task7.V8.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.KarnaukhovDA.Sprint2.Task7.V8.Lib
+{
+    public class DataService : ISprint2Task7V8
+    {
+        public bool CheckDotInShadedArea(double x, double y)
+        {
+            bool res;
+            if (y >= x * x && y <= 4 && x >= -2 && x <= 3)
+                {
+                res = true;
+                }
+            else
+            {
+                res = false;
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.KarnaukhovDA.Sprint2.Task7.V8.Test/DataServiceTest.cs
@@ -0,0 +1,228 @@
+﻿using Tyuiu.KarnaukhovDA.Sprint2.Task7.V8.Lib;
+namespace Tyuiu.KarnaukhovDA.Sprint2.Task7.V8.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCheckDotInShadedArea()
+        {
+            DataService ds = new DataService();
+
+            double x = 0.5;
+            double y = 3.5;
+
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+            bool wait = true;
+
+
+            Assert.AreEqual(res, wait);
+        }
+
+        [TestMethod]
+        public void ValidCheckDotInShadedArea2()
+        {
+            DataService ds = new DataService();
+
+            double x = 1.0;
+            double y = 0.5;
+
+           
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+            bool wait = false;
+
+           
+            Assert.AreEqual(res, wait);
+        }
+
+        [TestMethod]
+        public void ValidCheckDotInShadedArea3()
+        {
+            DataService ds = new DataService();
+
+            double x = 2.0;
+            double y = 4.0;
+
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+            bool wait = true;
+
+
+            Assert.AreEqual(res, wait);
+        }
+
+        [TestMethod]
+        public void ValidCheckDotInShadedArea4()
+        {
+            DataService ds = new DataService();
+
+            double x = -1.0;
+            double y = 4.0;
+
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+            bool wait = true;
+
+
+            Assert.AreEqual(res, wait);
+        }
+
+        [TestMethod]
+        public void ValidCheckDotInShadedArea5()
+        {
+            DataService ds = new DataService();
+
+            double x = -2.0;
+            double y = 2.5;
+
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+            bool wait = false;
+
+
+            Assert.AreEqual(res, wait);
+        }
+
+        [TestMethod]
+        public void ValidCheckDotInShadedArea6()
+        {
+            DataService ds = new DataService();
+
+            double x = 3.0;
+            double y = 4.0;
+
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+            bool wait = false;
+
+
+            Assert.AreEqual(res, wait);
+        }
+
+        [TestMethod]
+        public void ValidCheckDotInShadedArea7()
+        {
+            DataService ds = new DataService();
+
+            double x = 0.0;
+            double y = 5.0;
+
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+            bool wait = false;
+
+
+            Assert.AreEqual(res, wait);
+        }
+
+        [TestMethod]
+        public void ValidCheckDotInShadedArea8()
+        {
+            DataService ds = new DataService();
+
+            double x = -2.5;
+            double y = 3.0;
+
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+            bool wait = false;
+
+
+            Assert.AreEqual(res, wait);
+        }
+
+        [TestMethod]
+        public void ValidCheckDotInShadedArea9()
+        {
+            DataService ds = new DataService();
+
+            double x = 3.5;
+            double y = 3.0;
+
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+            bool wait = false;
+
+
+            Assert.AreEqual(res, wait);
+        }
+
+        [TestMethod]
+        public void ValidCheckDotInShadedArea10()
+        {
+            DataService ds = new DataService();
+
+            double x = -2.0;
+            double y = 4.0;
+
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+            bool wait = true;
+
+
+            Assert.AreEqual(res, wait);
+        }
+
+        [TestMethod]
+        public void ValidCheckDotInShadedArea11()
+        {
+            DataService ds = new DataService();
+
+            double x = 3.0;
+            double y = 4.0;
+
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+            bool wait = false;
+
+
+            Assert.AreEqual(res, wait);
+        }
+
+        [TestMethod]
+        public void ValidCheckDotInShadedArea12()
+        {
+            DataService ds = new DataService();
+
+            double x = 0.0;
+            double y = 0.0;
+
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+            bool wait = true;
+
+
+            Assert.AreEqual(res, wait);
+        }
+
+        [TestMethod]
+        public void ValidCheckDotInShadedArea13()
+        {
+            DataService ds = new DataService();
+
+            double x = -1.5;
+            double y = 3.0;
+
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+            bool wait = true;
+
+
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
